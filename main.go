88f4c893bccefa3c4dd68793c9ca3f15d74a20da
Michael Lieser <mlieser@anynines.com>
2025-06-11 09:17:30 +0200

    Service name

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ type PostgresqlCredentials struct {
 type VCAPServices struct {
 	PostgreSQL []struct {
 		Credentials PostgresqlCredentials `json:"credentials"`
-	} `json:"a9s-postgresql11"`
+	} `json:"a9s-postgresql17-ms-1749548047"`
 }
 
 type BlogPost struct {
